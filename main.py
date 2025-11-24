1d72e4b2f008d92b4c2a45315f699430a619e520
HeeM0508 <heemkill@gmail.com>
2025-11-24 14:07:14 +0300

    update main.py

--- main.py
+++ main.py
@@ -7,14 +7,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60)/1000
         screen.fill("black")
-        pygame.display.flip()
+        pygame.display.flip()    
+        
+        
 
 if __name__ == "__main__":
     main()
